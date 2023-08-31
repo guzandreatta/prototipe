c72e9796a471a7b07077280d550ba9e1a3474031
AstroPay <astropay@AstroPays-MacBook-Pro-2.local>
2023-08-31 11:03:53 -0300

    first commit

---
+++ data/story.js
@@ -0,0 +1,250 @@
+var story = {
+ "docName": "Events Prototype",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "Demo",
+ "layersExist": false,
+ "pages": [
+  {
+   "id": "6:808",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Home",
+   "image": "home.png",
+   "imageFixedLess": "fl/home.png",
+   "index": 0,
+   "width": 540,
+   "height": 720,
+   "x": -823,
+   "y": 1496,
+   "type": "regular",
+   "fixedPanels": [
+    {
+     "constrains": {
+      "top": false,
+      "bottom": false,
+      "left": false,
+      "right": false,
+      "height": false,
+      "width": false
+     },
+     "x": 0,
+     "y": 632,
+     "width": 540,
+     "height": 88,
+     "type": "float",
+     "index": 0,
+     "isFloat": true,
+     "isVertScroll": false,
+     "links": [],
+     "image": "home-0.png",
+     "mskH": 88
+    }
+   ],
+   "links": [
+    {
+     "rect": {
+      "x": 64,
+      "y": 320,
+      "width": 103,
+      "height": 32
+     },
+     "index": 0,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "6:2054",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Payments",
+   "image": "payments.png",
+   "index": 1,
+   "width": 540,
+   "height": 720,
+   "x": -235,
+   "y": 1496,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 24,
+      "y": 288,
+      "width": 200,
+      "height": 140
+     },
+     "index": 1,
+     "page": 2,
+     "disableAutoScroll": true
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "6:2755",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Payments(1)",
+   "image": "payments(1).png",
+   "index": 2,
+   "width": 540,
+   "height": 720,
+   "x": 397,
+   "y": 1496,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 24,
+      "y": 212,
+      "width": 492,
+      "height": 56
+     },
+     "index": 2,
+     "page": 3,
+     "disableAutoScroll": true
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "6:3028",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Payments(2)",
+   "image": "payments(2).png",
+   "index": 3,
+   "width": 540,
+   "height": 720,
+   "x": 1038,
+   "y": 1496,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 0,
+      "y": 505,
+      "width": 540,
+      "height": 214.62890625
+     },
+     "index": 3,
+     "page": 4,
+     "disableAutoScroll": true
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "6:16777",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Payments(3)",
+   "image": "payments(3).png",
+   "index": 4,
+   "width": 540,
+   "height": 720,
+   "x": 1656,
+   "y": 1496,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 106,
+      "y": 592,
+      "width": 327,
+      "height": 48
+     },
+     "index": 4,
+     "page": 5,
+     "disableAutoScroll": true
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "6:16970",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Payments(4)",
+   "image": "payments(4).png",
+   "index": 5,
+   "width": 540,
+   "height": 720,
+   "x": 2297,
+   "y": 1496,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 106,
+      "y": 592,
+      "width": 327,
+      "height": 48
+     },
+     "index": 5,
+     "page": 0
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "0:1",
+   "name": "Cyprus - InApp Payment Flow"
+  },
+  {
+   "id": "1:2",
+   "name": "Cyprus - OneTouch Flow"
+  },
+  {
+   "id": "1:3",
+   "name": "Barcelona - Deposit with Astro - INDIA"
+  },
+  {
+   "id": "1:4",
+   "name": "Barcelona - Deposit with Astro - BRAZIL"
+  },
+  {
+   "id": "1:5",
+   "name": "Barcelona - Deposit with PIX"
+  },
+  {
+   "id": "1:6",
+   "name": "Barcelona - InApp Payment Flow"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 6,
+ "backColor": "#FFFFFF"
+}
